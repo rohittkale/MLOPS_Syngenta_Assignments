3a046c5d1b5a9ff15e8014ce3f0175c9bf98c1c5
ROHIT KALE <98382567+rohittkale@users.noreply.github.com>
2024-11-23 18:44:14 +0530

    Update sentiment_analysis.py

--- Streamlit_Assignments (3)/sentiment_analysis.py
+++ Streamlit_Assignments (3)/sentiment_analysis.py
@@ -1,3 +1,8 @@
+#Problem Statement 3: Create a Streamlit Application for Basic Sentiment Analysis
+#Build a Streamlit application that takes user input in the form of text and performs basic sentiment analysis to determine whether the sentiment is positive, negative, or neutral.
+#Create a github repository, push your code, experiment with using multiple branches, and then merge your changes into the main branch.
+#Deploy your changes on Streamlit cloud
+
 import streamlit as st
 from textblob import TextBlob
 
